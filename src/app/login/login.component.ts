f26929162cb5b48139ec316389bf5ad3c253f75b
hebaArakha <hebatullah.ammar.rakha@gmail.com>
2024-04-25 13:56:41 +0200

    added : part of auth validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required)
+    })
+  }
+
+  handleLogin(){
+    console.log(this.loginForm);
+    
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,13 +1,52 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor() {
+    this.registerForm = new FormGroup({
+      fname: new FormControl('', Validators.required),
+      lname: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      pass1: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)]),
+      pass2: new FormControl('', [Validators.required, this.confirmPassword.bind(this)])
+    });
+  }
+
+  // Method to access pass1 value
+  get pass1Value() {
+    return this.registerForm.get('pass1')?.value;
+  }
+
+  // Method to access pass2 value
+  get pass2Value() {
+    return this.registerForm.get('pass2')?.value;
+  }
+
+  // confirmPassword method
+  confirmPassword(control: AbstractControl): ValidationErrors | null {
+    const confirmPassword = control.value;
+    const password = this.registerForm.get('pass1')?.value;
+
+    console.log(password);
+    console.log(confirmPassword);
+
+    if (password !== confirmPassword) {
+      return { passwordsNotMatching: true };
+    }
+    return null;
+  }
+
+  handleRegister() {
+    console.log(this.registerForm.controls);
+  }
 }
