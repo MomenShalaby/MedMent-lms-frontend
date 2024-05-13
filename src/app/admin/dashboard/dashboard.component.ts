8e7030679c6f57430d152423a3ee0057b5e93b3a
NadaNaguib <nada.naguib,asfour@gmail.com>
2024-05-13 21:33:08 +0300

    view courses

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -5,12 +5,14 @@ import { AddCourseComponent } from './add-course/add-course.component';
 import { AdminNavbarComponent } from '../admin-navbar/admin-navbar.component';
 import { RouterOutlet } from '@angular/router';
 import { UpdateCourseComponent } from './update-course/update-course.component';
+import { DisplayCourseComponent } from './display-course/display-course.component';
+import { CounterSectionComponent } from '../../counter-section/counter-section.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [SidebarComponent,AddEventComponent,AddCourseComponent,AdminNavbarComponent,RouterOutlet,
-    UpdateCourseComponent],
+    UpdateCourseComponent, DisplayCourseComponent, CounterSectionComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
